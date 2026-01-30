ef4f41b3e4d526f3c7e0b36cc49a053f411a4830
tuan <tuanhoang19042004@gmail.com>
2026-01-30 08:02:48 +0700

    Add gitignore and update application code

---
+++ check_models.py
@@ -0,0 +1,16 @@
+# check_models.py
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=api_key)
+
+print("--- DANH SÁCH MODEL BẠN ĐƯỢC DÙNG ---")
+try:
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(f"✅ {m.name}")
+except Exception as e:
+    print(f"❌ Lỗi Key: {e}")
